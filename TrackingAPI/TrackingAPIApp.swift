d148697e86da72c7084aceaf9f937ae03b9c2483
xmarano <leo.gregori@epitech.eu>
2024-07-03 01:23:36 +0200

    Initial Commit

---
+++ TrackingAPI/TrackingAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackingAPIApp.swift
+//  TrackingAPI
+//
+//  Created by Léo Grégori on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrackingAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
